9b7f2b429caeeef7bb5fc2240016f9528d8a98c8
ms5916 <ms5916@skbl-422-003.ph.ic.ac.uk>
2019-10-07 11:02:12 +0100

    Added comments to understand code simple 1

--- Simple1.asm
+++ Simple1.asm
@@ -8,14 +8,15 @@
 
 start
 	movlw 	0x0
-	movwf	TRISC, ACCESS	    ; Port C all outputs
-	bra 	test
-loop	movff 	0x06, PORTC
-	incf 	0x06, W, ACCESS
-test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x27
-	cpfsgt 	0x06, ACCESS
+	movwf	TRISC, ACCESS	    ; Port C all outputs, sets C to zero
+	bra 	test		    ; 
+loop	movff 	0x06, PORTC	    ; outputs, to port C
+	incf 	0x06, W, ACCESS	    ; +=1, moves f+1 into W memm
+test	movwf	0x06, ACCESS	    ; 06 arbitary memory location Test for end of loop condition
+	movlw 	0xFF		    ; sets literal value into w
+	cpfsgt 	0x06, ACCESS	    ; compare to file f, skip if greater than
 	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
+	goto 	0x0		    ; Re-run program from start, jumps back to org 0x0
 	
 	end
+; W special register - arithmetic operations, f file, l literal value, ACCESS BANK GOOD,
