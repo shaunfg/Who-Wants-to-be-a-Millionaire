c1364c849738a25b0648beafb8d4661c3dcaaabf
ms5916 <ms5916@skbl-422-003.ph.ic.ac.uk>
2019-10-17 15:15:54 +0100

    1st attempt

--- Simple1.asm
+++ Simple1.asm
@@ -6,79 +6,44 @@
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-main	;Set time for delay
-	movlw	0x03		    
-	movwf	0x20		    
-	
-	;Setup ports
-	movlw 	0x0		    
-	movwf	TRISD, ACCESS	    
-	movwf	TRISC, ACCESS	    ; Port C all outputs, sets C to zero, TRISC - sets into not 0/1, to control
-	movwf	TRISH, ACCESS	    
-	
-	;Resets output ports 
-	movlw	0x00		    
-	movwf	PORTE
-	movlw	0x00		    
-	movwf	PORTC
-	movlw	0x00		   
-	movwf	PORTH
-	
-	;Resets portD, so that no reading or writing
-	movlw	0x0f		    
-	movwf	PORTD
-	
-	;set E as output (to write into the memory)
-	movlw 	0x0		    ; Sets E as  output
-	movwf	TRISE, ACCESS	    ; Port C all outputs, sets C to zero, TRISC - sets into not 0/1, to control
-	bsf	PADCFG1,REPU, A	    ; Turn on pull-ups for Port E, 
-	
-	;write into memory 1
-	movlw	0xab		    ; stores arbitary value in E, for transfer
-	movwf	PORTE	    
-	movlw	0x0d		    ; Turns off CP1--> OE1 High only
-	movwf	PORTD
-	movff	0x20, 0x22	    ; prep delay
-	call	delay
-	movlw	0x0f		    ; Turns CP1,OE1 ON
-	movwf	PORTD
-	
-	;write into memory 2
-	movlw	0x54		    ; stores arbitary value in E, for transfer
-	movwf	PORTE	    
-	movlw	0x07		    ; Turns off CP2--> OE2 High only
-	movwf	PORTD
-	movff	0x20, 0x22	    ; prep delay
-	call	delay
-	movlw	0x0f		    ; Turns CP2,OE2 ON
-	movwf	PORTD
-	
-	;set E as input (to read the memory)
-	movlw	0xff		    ; sets E as input
-	movwf	TRISE, A	    ; Port E Direction Register
-	
-	;read memory 1
-	movlw	0x0e		    ; Sets OE1 low(for data read)
-	movwf	PORTD	
-	movff	PORTE,PORTC
-	;read memory 2
-	movlw	0x0b		    ; SETS OE2 low(for data read)
-	movwf	PORTD
-	movff	0x20, 0x22
-	call	delay
-	movff	PORTE,PORTH
-	
-	;resets Port D
-	movlw	0x0f		    ; Resets so no reading/ writing
-	movwf	PORTD
+
+main	call	SPI_MasterInit
+	movlw	0xab
+	call	SPI_MasterTransmit
+	call	Wait_Transmit
 	
 	goto	0x0
+
+
+SPI_MasterInit ; Set Clock edge to negative
+	bcf	SSP2STAT, CKE
+	; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+
+	movlw	(1<<SSPEN)|(1<<CKP)|(0x02)    ; tells how to consruct a byte ; SSPEN deefined, << shift left by SSPEN - 2*SSPEN, | logical or, setting 3 bits. 
+	movwf	SSP2CON1	    ;constructs a number, to set SSP2CON1 for a certain function. 
+	; written this way 
 	
+	; SDO2 output; SCK2 output
+	bcf	TRISD, SDO2		    ; SDO2 predefined as RD4??
+	bcf	TRISD, SCK2
+	return
+
+SPI_MasterTransmit ; Start transmission of data (held in W)
+	movwf	SSP2BUF		    ;moves byte from W to SFR
+	return
+
+Wait_Transmit ; Wait for transmission to complete - checks if transmission is complete
+	btfss	PIR2, SSP2IF	    ; PIR2 is a FR, SSP2IF is a bit in PIR2, skips if bit 'b' in register 'f' is 1, becomes one when SSP2BUF is finished.
+	bra	Wait_Transmit
+	bcf	PIR2, SSP2IF	    ; clear interrupt flag
+	return	
+
 delay	movff	0x20, 0x26
 	call	delay1
 	decfsz	0x22 ; decrement until zero
 	bra	delay
 	return
+	
 delay1	decfsz	0x26 ; decrement until zero
 	bra	delay1
 	return
