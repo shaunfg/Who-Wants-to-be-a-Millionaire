bb4c97d33dcd731fe6d1ecc2e617c9cd24e92432
ms5916 <ms5916@skbl-422-003.ph.ic.ac.uk>
2019-11-04 10:43:16 +0000

    Set Cursors Successfully

--- MAIN.asm
+++ MAIN.asm
@@ -3,29 +3,34 @@
 	extern	UART_Setup, UART_Transmit_Message   ; external UART subroutines
 	extern  LCD_Setup, LCD_Write_Message	    ; external LCD subroutines
 	extern	LCD_Write_Hex,LCD_Send_Byte_D	    ; external LCD subroutines
-	extern	Convert_Hex_Decimal,LCD_Set_Cursor
+;	extern	Convert_Hex_Decimal,LCD_Set_Cursor
 	extern	Key_In
-
-	extern ;delay, keypad, LCD
-
+	extern	LCD_Cursor_A,LCD_Cursor_B,LCD_Cursor_C,LCD_Cursor_D
+	
 	code
 	org 0x0
-	goto	setup
-
+	goto	start
+	
 	org 0x100		    ; Main code starts here at address 0x100
 
+	
+start	
+	bcf	EECON1, CFGS	; point to Flash program memory  
+	bsf	EECON1, EEPGD 	; access Flash program memory
+	call	UART_Setup	; setup UART
+	call	LCD_Setup	; setup LCD
+	
+	call	LCD_Cursor_A
+	call	LCD_Cursor_B
+	call	LCD_Cursor_C
+	call	LCD_Cursor_D
 
-    setup	
+	goto	start
+
 	;Questions in data structures
 	;Answers in data structures
 	;LCD initialisation
 	;UART initialisation
 	;Port D for LED control (prize)
-	bcf	EECON1, CFGS	; point to Flash program memory  
-	bsf	EECON1, EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
-	call	LCD_Setup	; setup LCD
-	goto	start
 
-	
 	end
