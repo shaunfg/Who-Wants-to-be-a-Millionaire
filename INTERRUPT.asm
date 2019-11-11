346d70f288903f90ec373084f21ffdbd5beaca3d
ms5916 <ms5916@skbl-422-003.ph.ic.ac.uk>
2019-11-11 16:46:30 +0000

    completed longer question implementation and interrupt to end game

--- INTERRUPT.asm
+++ INTERRUPT.asm
@@ -1,20 +1,38 @@
 	#include p18f87k22.inc
 
 	
-	global DAC_Setup
+	global DAC_setup
+	extern LCD_Clear_Display
     
+acs5	udata_acs   ; reserve data space in access ram
+timer	res 1
+save_w	res 1
+twenty	res 1
+	
 int_hi	code	0x0008		; high vector, no low vector
 	btfss	INTCON,TMR0IF	; check that this is timer0 interrupt
 	retfie	FAST		; if not then return
-	incf	LATD		; increment PORTD
+	movwf	save_w
+	
+	incf	timer		; increment PORTD
+	movlw	0x14		; 20 second timer!!!
+	cpfslt	timer
+	bra	end_game
 	bcf	INTCON,TMR0IF	; clear interrupt flag
+	movf	save_w,W
 	retfie	FAST		; fast return from interrupt
 
 DAC	code
 
+end_game   
+	call	LCD_Clear_Display
+	return
+	
 DAC_setup
-	clrf	TRISD		; Set PORTD as all outputs
-	clrf	LATD		; Clear PORTD outputs
+
+	movlw	0x0
+	movwf	timer
+	
 	movlw	b'10000111'	; Set timer0 to 16-bit, Fosc/4/256
 	movwf	T0CON		; = 62.5KHz clock rate, approx 1sec rollover
 	bsf	INTCON,TMR0IE	; Enable timer0 interrupt
