78d4a7f19b3881ba0926db3d2e9d5915019bc888
ms5916 <ms5916@skbl-422-003.ph.ic.ac.uk>
2019-11-12 15:52:04 +0000

    Adding Buzzer feature, i believe we have set up PWM for operation, need to figure out how to send the signal to the piezo buzzer?

---
+++ BUZZER.asm
@@ -0,0 +1,29 @@
+	#include p18f87k22.inc
+
+	global	Buzzer_Setup
+buzzer	code
+	
+Buzzer_Setup
+	
+	movlw	b'11111111'
+	movwf	PR2
+	
+	movlw	b'00101100';single output, PWM duty cycle, PWM mode
+	movwf	CCP4CON
+	
+	movlw	0xab
+	movwf	CCPR4L
+	
+	;movlw	0xab
+	;movwf	CCP4CON<5:4>
+	
+	movlw	0x0
+	movwf	TRISB
+	
+	movlw	b'10000011'	; Set timer0 to 16-bit, Fosc/4/256
+	movwf	T2CON		; = 62.5KHz clock rate, approx 1sec rollover
+	bsf	INTCON,TMR2IE	; Enable timer0 interrupt
+	bsf	INTCON,GIE	; Enable all interrupts
+	
+	
+end
